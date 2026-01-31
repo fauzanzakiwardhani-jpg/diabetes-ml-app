7f54d3517dfccf77888aab2955ec7304118b9354
Fauzan Zaki Putra Wardhani <fauzanzakiwardhani@gmail.com>
2026-01-31 10:20:18 +0700

    Add numpy import to app.py

--- app.py
+++ app.py
@@ -9,9 +9,9 @@ Original file is located at
 
 import streamlit as st
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 
-from modul import NamaClass
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler,LabelEncoder
 from sklearn.linear_model import LogisticRegression
